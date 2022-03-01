8393e8b6638d0217bfa60f328bbe6cd4b61d3d20
13120dde <13120dde@gmail.com>
2022-03-01 20:26:59 +0100

    aaa

--- Scripts/GetDevices.ps1
+++ Scripts/GetDevices.ps1
@@ -1,5 +1,12 @@
-write-host "get devices to upgrade"
-
-
-
-write-host "saving device file"
+$Devices = @(
+    @{
+        type='standardcamera'
+        ip='192.168.0.1'
+    },
+    @{
+        type='minicamera'
+        ip='192.168.0.2'
+    }
+)
+ConvertTo-Json -InputObject $Devices
+$Devices| ConvertTo-Json | Out-File "deviceInfo.json"
