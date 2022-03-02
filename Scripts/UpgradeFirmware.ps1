3f57130659916bf00026337b6009bbfbf8e3e727
13120dde <13120dde@gmail.com>
2022-03-02 16:44:15 +0100

    pre script steps done

--- Scripts/UpgradeFirmware.ps1
+++ Scripts/UpgradeFirmware.ps1
@@ -1,2 +1,11 @@
-$default_site = $args[0]
-write-host $default_site
+$atConfiguration = $args[0]
+$siteName = $args[1]
+$user = $args[2]
+$password = $args[3]
+
+Write-Host "Starting FW upgrade script for following configuration:"
+
+write-host "\t AT: " + $atConfiguration
+write-host "\t Site: " + $siteName
+write-host "\t User: " + $user
+write-host "\tPassword: " + $password
