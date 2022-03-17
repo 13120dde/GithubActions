5fc537c0502892549f0663ee5903bd00b2413569
Patrik <patrik@oresunditconsulting.se>
2022-03-17 16:19:15 +0100

    testing bnet auth from script

---
+++ Scripts/GetBnetToken.ps1
@@ -0,0 +1,13 @@
+﻿
+$ClientID = $args[0]
+$ClientSecret = $args[1]
+
+$Uri = "https://eu.battle.net/oauth/token"
+$FormRequest = @{
+    client_id  = $ClientID
+    client_secret  = $ClientSecret
+    grant_type = 'client_credentials'
+}
+$Result = Invoke-WebRequest -Uri $Uri -Method Post -Body $FormRequest
+$token = ConvertFrom-Json $Result.Content
+return $token.access_token
--- Scripts/SomeFile.ps1
+++
@@ -1,8 +0,0 @@
-﻿$Uri = "https://us.battle.net/oauth/token"
-$Form = @{
-    client_id  = 'b2398853129540a9b736381f2b65007e'
-    client_secret  = '2X8z6QpMhfXQuXwaqW6Dq9iDpX7Ci9pN'
-    grant_type = 'client_credentials'
-}
-$Result = Invoke-RestMethod -Uri $Uri -Method Post -Form $Form
-Write-Host $Result
