d81141bdd3b0d5f4895b5874c1516339e27c2b89
Patrik <patrik@oresunditconsulting.se>
2022-03-18 13:59:57 +0100

    1

--- Scripts/GetBnetToken.ps1
+++ Scripts/GetBnetToken.ps1
@@ -1,9 +1,14 @@
 ﻿
-$ClientID = $args[0]
-$ClientSecret = $args[1]
-$Region = $args[2]
-Write-Host "region $Region"
-$Url = "https://"+$Region+".battle.net/oauth/token"
+#$ClientID = $args[0]
+#$ClientSecret = $args[1]
+#$Realm = $args[2]
+
+$ClientID = "b2398853129540a9b736381f2b65007e"
+$ClientSecret = "2X8z6QpMhfXQuXwaqW6Dq9iDpX7Ci9pN"
+
+#$Url = "https://"+$Region+".battle.net/oauth/token"
+
+$Url = "https://eu.battle.net/oauth/token"
 $RequestBody = @{
     client_id  = $ClientID
     client_secret  = $ClientSecret
@@ -11,4 +16,6 @@ $RequestBody = @{
 }
 $Result = Invoke-WebRequest -Uri $Url -Method Post -Body $RequestBody
 $Token = ConvertFrom-Json $Result.Content
+
+Write-Host $Token
 return $Token.access_token
--- Scripts/GetCharactersFromServer.ps1
+++ Scripts/GetCharactersFromServer.ps1
@@ -1,6 +1,29 @@
 ﻿
-$AccessToken = $args[0]
-$Region = $args[1]
-Write-Host "Regions: $Region"
-Write-Host "accToken $AccessToken"
+#$AccessToken = "Bearer $args[0]"
+#$Region = $args[1]
+#$GAMER_PROFILE =[System.Web.HTTPUtility]::UrlEncode($args[2])
+
+$AccessToken = "EUFSlH3SMa8N5qJiCVv7RixovrezD8tySk"
+
+$Region = "eu"
+$GAMER_PROFILE = [System.Web.HTTPUtility]::UrlEncode("Brodde#2647")
+
+
+$Url = "https://$Region.api.blizzard.com/d3/profile/$GAMER_PROFILE/?locale=en_US"
+Write-Host $Url
+
+$Result = Invoke-WebRequest -Uri $Url -Method Get -Body $RequestBody -Headers @{'Authorization' = "Bearer US0HEZzeN79S5oqPMDyL2Ds2E0i9Ahe5jv"}
+
+$ResultContent = ConvertFrom-Json $Result.Content
+$Heroes = $ResultContent.heroes
+
+$HeroesParsed = @()
+foreach($Hero in $Heroes){
+    $HeroesParsed += [PSCustomObject] @{
+        Id = $Hero.id
+        ClassSlug = $Hero.classSlug
+    }
+
+}
+return ConvertTo-Json $HeroesParsed
 
