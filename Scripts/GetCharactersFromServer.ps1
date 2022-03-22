a33a5e5bb61b9978bbed1684950b63d3b60f0b34
Patrik <patrik@oresunditconsulting.se>
2022-03-22 13:19:56 +0100

    1

--- Scripts/GetCharactersFromServer.ps1
+++ Scripts/GetCharactersFromServer.ps1
@@ -1,15 +1,15 @@
-﻿$AccessToken = "Bearer "+$args[0]
-#$AccessToken = "Bearer EUFSlH3SMa8N5qJiCVv7RixovrezD8tySk"
+﻿$AT = "Bearer "+$args[0]
+#$AT = "Bearer EUFSlH3SMa8N5qJiCVv7RixovrezD8tySk"
 $Region = $args[1]
 #$Region ="eu"
 $GAMER_PROFILE =[System.Web.HTTPUtility]::UrlEncode($args[2])
 #$GAMER_PROFILE ="Brodde%232647"
 $Url = "https://$Region.api.blizzard.com/d3/profile/$GAMER_PROFILE/?locale=en_US"
 Write-Host $Url
-Write-Host "accToken: $AccessToken"
+Write-Host "at: $AT"
 
-$Result = Invoke-WebRequest -Uri $Url -Method Get -Headers @{'Authorization' = $AccessToken}
-
+$Result = Invoke-WebRequest -Uri $Url -Method Get -Headers @{'Authorization' = $AT}
+$Result
 $ResultContent = ConvertFrom-Json $Result.Content
 $Heroes = $ResultContent.heroes
 
@@ -17,12 +17,14 @@ $HeroesParsed = @()
 foreach($Hero in $Heroes){
     $HeroesParsed += [PSCustomObject] @{
         Id = $Hero.id
-        ClassSlug = $Hero.ClassSlug
+        ClassSlug = $Hero.classSlug
 
     }
 }
-
+Write-Host "heroes parsed: $HeroesParsed"
 $Matrix = [PSCustomObject]@{
     include = $HeroesParsed
 }
+
+Write-Host "matrix: $Matrix"
 return ConvertTo-Json  -Compress -InputObject $Matrix
