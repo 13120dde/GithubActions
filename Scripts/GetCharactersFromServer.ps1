1571f2953455297a9a505827d4a23de086148124
Patrik <patrik@oresunditconsulting.se>
2022-03-22 13:07:17 +0100

    1

--- Scripts/GetCharactersFromServer.ps1
+++ Scripts/GetCharactersFromServer.ps1
@@ -1,11 +1,13 @@
 ﻿
 $AccessToken = "Bearer "+$args[0]
+#$AccessToken = "Bearer EUFSlH3SMa8N5qJiCVv7RixovrezD8tySk"
 $Region = $args[1]
+#$Region ="eu"
 $GAMER_PROFILE =[System.Web.HTTPUtility]::UrlEncode($args[2])
+#$GAMER_PROFILE ="Brodde%232647"
 $Url = "https://$Region.api.blizzard.com/d3/profile/$GAMER_PROFILE/?locale=en_US"
-
 Write-Host $Url
-Write-Host "acc: $AccessToken"
+Write-Host "accToken: $AccessToken"
 
 $Result = Invoke-WebRequest -Uri $Url -Method Get -Headers @{'Authorization' = $AccessToken}
 
@@ -15,15 +17,14 @@ $Heroes = $ResultContent.heroes
 $HeroesParsed = @()
 foreach($Hero in $Heroes){
     $HeroesParsed += [PSCustomObject] @{
-        Id = $Hero.id
-        ClassSlug = $Hero.ClassSlug
-
+        Id = $Hero.id.ToString()
     }
 }
 
-$Matrix = [PSCustomObject]@{
+$myObject = [PSCustomObject]@{
     include = $HeroesParsed
 }
- return ConvertTo-Json  -Compress -InputObject $Matrix
+Write-Host $myObject
+return ConvertTo-Json  -Compress -InputObject $myObject
 
 
