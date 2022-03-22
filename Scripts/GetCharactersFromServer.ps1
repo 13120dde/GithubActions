61edcd1d24e8176ab6795dc6c2acfb95b9c267b7
Patrik <patrik@oresunditconsulting.se>
2022-03-22 09:14:17 +0100

    1

--- Scripts/GetCharactersFromServer.ps1
+++ Scripts/GetCharactersFromServer.ps1
@@ -1,13 +1,10 @@
 ﻿
 $AccessToken = "Bearer "+$args[0]
-#$AccessToken = "Bearer EUFSlH3SMa8N5qJiCVv7RixovrezD8tySk"
 $Region = $args[1]
-#$Region ="eu"
 $GAMER_PROFILE =[System.Web.HTTPUtility]::UrlEncode($args[2])
-#$GAMER_PROFILE ="Brodde%232647"
 $Url = "https://$Region.api.blizzard.com/d3/profile/$GAMER_PROFILE/?locale=en_US"
+
 Write-Host $Url
-Write-Host "accToken: $AccessToken"
 
 $Result = Invoke-WebRequest -Uri $Url -Method Get -Headers @{'Authorization' = $AccessToken}
 
---
+++ Scripts/WireCharacterData.ps1
@@ -0,0 +1,18 @@
+﻿$Region = $args[0]
+$GAMER_PROFILE =[System.Web.HTTPUtility]::UrlEncode($args[1])
+$AccessToken = "Bearer "+$args[2]
+$HeroID = $args[3]
+$ClassSlug = $args[4]
+
+Write-Host $Region
+Write-Host $GAMER_PROFILE
+Write-Host $AccessToken
+Write-Host $HeroID
+Write-Host $ClassSlug
+
+
+$Region = "eu"
+$GAMER_PROFILE ="Brodde%232647"
+$AccessToken ="Bearer EUrOo6zeFePgP7LFps3aiS0mrHNGPeRqYH"
+$HeroID = 134165191
+$ClassSlug = "crusader"
