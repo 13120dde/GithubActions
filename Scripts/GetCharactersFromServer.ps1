ef44a5fc71f8edb3df52f8acfaeb84d7593e163c
Patrik <patrik@oresunditconsulting.se>
2022-03-22 13:49:55 +0100

    1

--- Scripts/GetCharactersFromServer.ps1
+++ Scripts/GetCharactersFromServer.ps1
@@ -1,14 +1,14 @@
-﻿$AT = "Bearer "+$args[0]
-#$AT = "Bearer EUFSlH3SMa8N5qJiCVv7RixovrezD8tySk"
+﻿$AccessToken = "Bearer "+$args[0]
+#$AccessToken = "Bearer EUFSlH3SMa8N5qJiCVv7RixovrezD8tySk"
 $Region = $args[1]
 #$Region ="eu"
 $GAMER_PROFILE =[System.Web.HTTPUtility]::UrlEncode($args[2])
 #$GAMER_PROFILE ="Brodde%232647"
 $Url = "https://$Region.api.blizzard.com/d3/profile/$GAMER_PROFILE/?locale=en_US"
 Write-Host $Url
-Write-Host "at: $AT"
+Write-Host "at: $AccessToken"
 
-$Result = Invoke-WebRequest -Uri $Url -Method Get -Headers @{'Authorization' = $AT}
+$Result = Invoke-WebRequest -Uri $Url -Method Get -Headers @{'Authorization' = $AccessToken}
 $Result
 $ResultContent = ConvertFrom-Json $Result.Content
 $Heroes = $ResultContent.heroes
@@ -16,7 +16,7 @@ $Heroes = $ResultContent.heroes
 $HeroesParsed = @()
 foreach($Hero in $Heroes){
     $HeroesParsed += [PSCustomObject] @{
-        Id = $Hero.id
+        Id = $Hero.id.ToString()
         ClassSlug = $Hero.classSlug
 
     }
