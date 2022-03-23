204fe288151e49dd8786983fd5cfa912c29f8c66
Patrik <patrik@oresunditconsulting.se>
2022-03-23 11:24:22 +0100

    1

--- Scripts/ProcessCharacterData.ps1
+++ Scripts/ProcessCharacterData.ps1
@@ -17,3 +17,25 @@ foreach($File in Get-ChildItem $ArtilfactFolder)
 }
 
 Write-Host $HighestHero
+$Message = "### HIGHEST  CHARACTER###`n`n"
+$Message += "Name: "+$HighestHero.Name+"`n"
+$Message += "Class: "+$HighestHero.Class+"`n"
+$Message += "Elite Kills: "+$HighestHero.EliteKills+"`n`n"
+$Message += "Stats: `n"
+
+$ItemTableRaw =@{}
+$HighestHero.Stats.psobject.Properties | foreach {$ItemTableRaw[$_.Name] = $_.Value}
+foreach($Item in $ItemTableRaw.GetEnumerator())
+{
+    $Message +="`t"+$Item.Key+": "+$Item.Value+"`n"
+}
+$Message += "Gear:`n"
+$ItemTableRaw =@{}
+$HighestHero.Gear.psobject.Properties | foreach {$ItemTableRaw[$_.Name] = $_.Value}
+foreach($Item in $ItemTableRaw.GetEnumerator())
+{
+    $GearSummary = $Item.Value.Name+" - "+$Item.Value.TypeName
+    $Message +="`t"+$Item.Key+": "+$GearSummary+"`n"
+}
+
+Write-Host $Message
