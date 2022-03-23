15b8d584aeb02d1d98475f446c7b86db962fba20
Patrik <patrik@oresunditconsulting.se>
2022-03-23 10:18:40 +0100

    1

---
+++ Scripts/ProcessCharacterData.ps1
@@ -0,0 +1,19 @@
+﻿$CurrentDir = Get-Location
+$CurrentDirParent = (Get-Item $CurrentDir).Parent.FullName
+$ArtilfactFolder = $CurrentDirParent.ToString()+"\artifacts\characters\"
+Write-Host "Artifact folder: $ArtilfactFolder"
+
+$HighestKills = 0
+$HighestHero = $null
+foreach($File in Get-ChildItem $ArtilfactFolder)
+{
+    # Processing code goes here
+    $Hero = Get-Content -Raw -Path $File.FullName | ConvertFrom-Json
+    if($Hero.EliteKills -gt $HighestKills)
+    {
+        $HighestKills = $Hero.EliteKills
+        $HighestHero = $Hero
+    }
+}
+
+Write-Host $HighestHero
